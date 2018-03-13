580620834b999448081a24f71ec542712f473c3a
Nicolas Zorzano <nico.zorzano@live.com.ar>
2018-03-13 20:40:40 -0300

    Complete database classes

--- utils/Database.py
+++ utils/Database.py
@@ -4,6 +4,7 @@ import random
 import string
 import python_speech_features as features
 from typing import List
+import pickle
 
 
 class Label:
@@ -14,88 +15,118 @@ class Label:
 
     def __init__(self, transcription: str):
         self.text: str = transcription
-        self.targets = transcription.replace(' ', '  ').split(' ')
-        self.indeces = self.toIndex()
+        # Delete blanks at the beginning and the end of the transcription, transform to lowercase,
+        # delete numbers in the beginning, etc.
+        self.targets = (' '.join(transcription.strip().lower().split(' ')[2:]).replace('.', '')).replace(' ', '  ').split(' ')
+        self.indices = None
 
-    def toIndex(self):
-        # Adding blank label
-        index = np.hstack([self.SPACE_TOKEN if x == '' else list(x) for x in self.targets])
+    def toIndex(self) -> np.ndarray:
+        if self.indices is None:
+            # Adding blank label
+            index = np.hstack([self.SPACE_TOKEN if x == '' else list(x) for x in self.targets])
+            # Transform char into index
+            index = np.asarray([self.SPACE_INDEX if x == '<space>' else ord(x) - self.FIRST_INDEX for x in index])
+            return index
+        else:
+            return self.indices
 
-        # Transform char into index
-        index = np.asarray([self.SPACE_INDEX if x == '<space>' else ord(x) - self.FIRST_INDEX for x in index])
-        return index
+    def __str__(self):
+        return str(self.indices)
 
     @staticmethod
     def fromFile(file_name: str):
         with open(file_name, 'r') as f:
             transcription = f.readlines()[0]  # This method assumes that the transcription is in the first line
-            # Delete blanks at the begining and the end of the transcription, transform to lowercase, etc.
-            transcription = ' '.join(transcription.strip().lower().split(' ')[2:]).replace('.', '')
 
             return Label(transcription)  # Create Label class from transcription
 
-    def __str__(self):
-        return str(self.indeces)
+
+class AudioFeature:
+    def __init__(self, audio: np.ndarray, fs: float, normalize_audio=True):
+
+        if normalize_audio:
+            self.audio = audio / abs(max(audio))
+        else:
+            self.audio = audio
+        self.fs = fs
+
+    def getMfcc(self,
+                winlen: float =0.2,
+                winstep: float=0.1,
+                numcep: int=13,
+                nfilt: int =40,
+                nfft: int =1024,
+                lowfreq=0,
+                highfreq=None,
+                preemph: float =0.98) -> np.ndarray:
+
+        return features.mfcc(self.audio, samplerate=self.fs, winlen=winlen, winstep=winstep, numcep=numcep,
+                             nfilt=nfilt, nfft=nfft, lowfreq=lowfreq, highfreq=highfreq, preemph=preemph)
+
+    def getSpectrogram(self) -> np.ndarray:
+        raise NotImplementedError("Should have implemented this")   # TODO implement spectrogram as feature
+
+    @staticmethod
+    def fromFile(wav_name: str, normalize_audio=True):
+        # Read the wav file
+        signal, fs = librosa.load(wav_name)
+        return AudioFeature(signal, fs, normalize_audio)
 
 
-class DatabaseItem(Label):
+class DatabaseItem(Label, AudioFeature):
     def __copy__(self):
         return self
 
-    def __init__(self, mfcc: np.ndarray, label: Label):
-        self.mfcc = mfcc
+    def __init__(self, feature: AudioFeature, label: Label):
+        self.feature = feature
         self.label = label
-        self.n_mfcc = np.shape(mfcc)[1]
-        self.n_frames = np.shape(mfcc)[0]
 
-    def getNumMfcc(self) -> int:
-        return self.n_mfcc
+    def getFeature(self) -> AudioFeature:
+        return self.feature
 
-    def getNumFrames(self) -> int:
-        return self.n_frames
+    def getMfcc(self,
+                winlen: float = 0.2,
+                winstep: float = 0.1,
+                numcep: int = 13,
+                nfilt: int = 40,
+                nfft: int = 1024,
+                lowfreq=0,
+                highfreq=None,
+                preemph: float = 0.98) -> np.ndarray:
 
-    def getMfcc(self) -> np.ndarray:
-        return self.mfcc
+        return self.feature.getMfcc(
+            winlen=winlen, winstep=winstep, numcep=numcep, nfilt=nfilt, nfft=nfft,
+            lowfreq=lowfreq, highfreq=highfreq, preemph=preemph)
+
+    def getSpectrogram(self,
+                       winlen: float = 0.2,
+                       winstep: float = 0.1,
+                       numcep: int = 13,
+                       nfilt: int = 40,
+                       nfft: int = 1024,
+                       lowfreq=0,
+                       highfreq=None,
+                       preemph: float = 0.98) -> np.ndarray:                   # TODO implement spectrogram as feature
+
+        return self.feature.getSpectrogram()
 
     def getLabel(self) -> Label:
         return self.label
 
-    def __str__(self):
-        return 'Label: '+str(self.label)+'\n' + 'Data: '+str(self.mfcc)
+    def getLabelIndices(self) -> np.ndarray:
+        return self.label.toIndex()
+
+    # def __str__(self):
+    #     return 'Label: '+str(self.label)+'\n' + 'Data: '+str(self.mfcc)
 
     @staticmethod
-    def fromFile(
-            wav_name: str,
-            label_name: str,
-            winlen: float =0.2,
-            winstep: float=0.1,
-            numcep: int=13,
-            nfilt: int =40,
-            nfft: int =1024,
-            lowfreq=0,
-            highfreq=None,
-            preemph: float =0.98
-    ):
+    def fromFile(wav_name: str, label_name: str):
         # Get label
         label = Label.fromFile(label_name)
-        # Read the wav file
-        signal, fs = librosa.load(wav_name)
-        # Normalize audio
-        signal = signal / abs(max(signal))
-        # Get the MFCCs coefficients. The size of the matrix is n_mfcc x T, so the dimensions
-        # are not the same for every sample
-        mfcc = features.mfcc(signal,
-                             samplerate=fs,
-                             winlen=winlen,
-                             winstep=winstep,
-                             numcep=numcep,
-                             nfilt=nfilt,
-                             nfft=nfft,
-                             lowfreq=lowfreq,
-                             highfreq=highfreq,
-                             preemph=preemph)
+        # Get features
+        feature = AudioFeature.fromFile(wav_name)
 
-        return DatabaseItem(mfcc, label)
+        return DatabaseItem(feature, label)
 
 
 class Database(DatabaseItem):
@@ -120,34 +151,38 @@ class Database(DatabaseItem):
             mfcc_list.append(self.__database[_].getMfcc())
         return mfcc_list
 
+    def getSpectrogramList(self) -> List[np.ndarray]:
+        spectrogram_list = []
+        for _ in range(self.length):
+            spectrogram_list.append(self.__database[_].getSpectrogram())
+        return spectrogram_list
+
     def getLabelsList(self) -> List[Label]:
         label_list = []
         for _ in range(self.length):
             label_list.append(self.__database[_].getLabel())
         return label_list
 
-    # def create_batch_plan(self):
-    #     self.batch_plan = self.mfccDatabase
-    #     random.shuffle(self.batch_plan)
-    #     self.batch_count = 0
-    #
-    # def next_batch(self):
-    #     if self.batch_count == 0:
-    #         self.create_batch_plan()
-    #
-    #     start_index = self.batch_size * self.batch_count
-    #     end_index = start_index + self.batch_size
-    #     self.batch_count += 1
-    #     if end_index >= len(self.batch_plan):
-    #         end_index = len(self.batch_plan)
-    #         start_index = end_index - self.batch_size
-    #         self.batch_count = 0
-    #
-    #     data = Database(self.batch_plan[start_index:end_index], self.batch_size)
-    #
-    #     return data.getData(), data.getLabels()    #TODO CHEACKEAR ESTO
+    def create_batch_plan(self):
+        self.batch_plan = self.__database
+        random.shuffle(self.batch_plan)
+        self.batch_count = 0
 
-    def getItemFromIndex(self, index):
+    def next_batch(self):
+        if self.batch_count == 0:
+            self.create_batch_plan()
+
+        start_index = self.batch_size * self.batch_count
+        end_index = start_index + self.batch_size
+        self.batch_count += 1
+        if end_index >= len(self.batch_plan):
+            end_index = len(self.batch_plan)
+            start_index = end_index - self.batch_size
+            self.batch_count = 0
+
+        return self.getRange(start_index, end_index)    # TODO Check this method
+
+    def getItemFromIndex(self, index) -> DatabaseItem:
         if index > self.length:
             return None
         return self.__database[index]
@@ -157,14 +192,26 @@ class Database(DatabaseItem):
             return None
         return Database.fromList(self.__database[start_index:end_index], self.batch_size)
 
+    def __len__(self):
+        return self.length
+
+    def save(self, file_name):
+        file = open(file_name, 'wb')
+        pickle.dump(self, file, pickle.HIGHEST_PROTOCOL)
+        file.close()
+
     @staticmethod
-    def fromList(list, batch_size: int = 50):
+    def fromList(input_list: List[DatabaseItem], batch_size: int = 50):
         database = Database(batch_size=batch_size)
-        for _ in range(len(list)):
-            database.append(list[_])
+        for _ in range(len(input_list)):
+            database.append(input_list[_])
 
         return database
 
-    def __len__(self):
-        return self.length
-    
+    @staticmethod
+    def fromFile(file_name: str):
+        # Load the database
+        file = open(file_name, 'rb')
+        data = pickle.load(file)
+        file.close()
+        return data
--- utils/GenerateDatabase.py
+++ utils/GenerateDatabase.py
@@ -1,10 +1,6 @@
-import librosa
-import pickle
 import os
 from utils.Database import Database, DatabaseItem
 import matplotlib.pyplot as plt
-import numpy as np
-import python_speech_features as features
 
 # Printing parameteres
 show_figures = False
@@ -36,17 +32,12 @@ for wav_index in range(len(wav_names)):
     
     # Create database item
     item = DatabaseItem.fromFile(wav_name=wav_filename,
-                                 label_name=label_filename,
-                                 winlen=frame_length,
-                                 winstep=frame_stride,
-                                 numcep=n_mfcc,
-                                 nfilt=num_filters,
-                                 nfft=fft_points,
-                                 lowfreq=0,
-                                 highfreq=None,
-                                 preemph=preemphasis_coeff)
-    print(item.mfcc)
-    print(item.label)
+                                 label_name=label_filename)
+    print(
+        item.getMfcc(winlen=frame_length, winstep=frame_stride, numcep=n_mfcc, nfilt=num_filters,
+                     nfft=fft_points, lowfreq=0, highfreq=None, preemph=preemphasis_coeff)
+    )
+    print(item.label.targets)
     # Add the new data to the database
     database.append(item)
 
@@ -54,7 +45,7 @@ for wav_index in range(len(wav_names)):
     if wav_index == 0 and show_figures:
         plt.figure(num=figure, figsize=(2, 2))
         figure = figure + 1
-        heatmap = plt.pcolor(mfcc)
+        heatmap = plt.pcolor(item.getMfcc())
         plt.title(wav_names[wav_index])
         plt.draw()
 
@@ -62,11 +53,14 @@ for wav_index in range(len(wav_names)):
 
 if show_figures:
     plt.show()
-# # Save the database into a file
-# file = open(OUT_DIR + '/' + OUT_FILE, 'wb')
-# # Trim the samples to a fixed length
-# pickle.dump(database.print(), file)
-# file.close()
+
+# Save the database into a file
+database.save(OUT_DIR + '/' + OUT_FILE)
+
+# Load the database
+database2 = Database.fromFile(OUT_DIR + '/' + OUT_FILE)
+print(database.getItemFromIndex(0).getLabelIndices())
+print(database2.getItemFromIndex(0).getLabelIndices())
 
 print("Database generated")
 print("Number of elements in database: " + str(database.length))
