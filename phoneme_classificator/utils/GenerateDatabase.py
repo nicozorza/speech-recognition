5ec62564a4cedb03b2e80cf31aec771ac82e6619
nicozorza <tecnico2011>
2018-06-20 22:48:18 -0300

    Change some parameters to fit a paper description

--- phoneme_classificator/utils/GenerateDatabase.py
+++ phoneme_classificator/utils/GenerateDatabase.py
@@ -19,12 +19,15 @@ feature_lengths = []
 # Configuration of the features
 feature_config = FeatureConfig()
 feature_config.feature_type = 'mfcc'
-feature_config.nfft = 1024
-feature_config.winlen = 20
+feature_config.nfft = 512
+feature_config.winlen = 25
 feature_config.winstride = 10
 feature_config.preemph = 0.98
-feature_config.num_filters = 48
-feature_config.num_ceps = 26
+feature_config.num_filters = 26
+feature_config.num_ceps = 14
+feature_config.ceplifter = 22
+feature_config.appendEnergy = True
+feature_config.winfunc = np.hamming
 
 # Load project data
 project_data = ProjectData()
