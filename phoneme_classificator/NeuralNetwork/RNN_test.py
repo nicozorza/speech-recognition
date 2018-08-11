692e5aa2c29cb764babff1737683911341cb211e
nicozorza <tecnico2011>
2018-08-11 14:54:03 -0300

    Reached 71.5% with this model

--- phoneme_classificator/NeuralNetwork/RNN_test.py
+++ phoneme_classificator/NeuralNetwork/RNN_test.py
@@ -18,30 +18,34 @@ network_data.tensorboard_path = project_data.TENSORBOARD_PATH
 network_data.num_classes = 63
 network_data.num_features = 26
 
-network_data.num_input_dense_layers = 0
-network_data.num_input_dense_units = [100] * network_data.num_input_dense_layers
+network_data.num_input_dense_layers = 2
+network_data.num_input_dense_units = [250, 150]
 network_data.input_dense_activations = [tf.nn.tanh] * network_data.num_input_dense_layers
+network_data.input_batch_normalization = True
 
 network_data.is_bidirectional = True
-network_data.rnn_regularizer = 0.2
-network_data.num_fw_cell_units = [64]
-network_data.num_bw_cell_units = [8]
+network_data.rnn_regularizer = 0.09
+network_data.num_fw_cell_units = [240, 160]
+network_data.num_bw_cell_units = [150, 30]
 network_data.cell_fw_activation = [tf.nn.tanh] * len(network_data.num_fw_cell_units)
 network_data.cell_bw_activation = [tf.nn.tanh] * len(network_data.num_bw_cell_units)
 
-network_data.num_dense_layers = 0
-network_data.num_dense_units = [100] * network_data.num_dense_layers
+network_data.num_dense_layers = 1
+network_data.num_dense_units = [150]
 network_data.dense_activations = [tf.nn.tanh] * network_data.num_dense_layers
-network_data.dense_regularizer = 0.2
+network_data.dense_regularizer = 0.3
+network_data.dense_batch_normalization = True
 
-network_data.out_activation = tf.nn.relu
+network_data.out_activation = None
 network_data.out_regularizer_beta = 0.0
 network_data.out_regularizer = l2_regularizer(network_data.out_regularizer_beta)
 
-network_data.keep_dropout = None
+network_data.use_dropout = True
+network_data.keep_dropout_input = [0.8, 0.8]
+network_data.keep_dropout_output = [0.8]
 
-network_data.learning_rate = 0.01
-network_data.adam_epsilon = 0.01
+network_data.learning_rate = 0.001
+network_data.adam_epsilon = 0.005
 network_data.optimizer = tf.train.AdamOptimizer(learning_rate=network_data.learning_rate,
                                                 epsilon=network_data.adam_epsilon)
 ###########################################################################################################
@@ -64,7 +68,7 @@ network.train(
     save_freq=10,
     use_tensorboard=True,
     tensorboard_freq=10,
-    training_epochs=1,
+    training_epochs=100,
     batch_size=50
 )
 
